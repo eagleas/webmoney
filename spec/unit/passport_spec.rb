700ef9ab8b9d8ee89a9690bf1e275b4c11543d7e
Alex Eagle <eagle.alex@gmail.com>
2010-01-18 14:30:05 +0300

    Add new fields to attestat

--- spec/unit/passport_spec.rb
+++ spec/unit/passport_spec.rb
@@ -16,70 +16,76 @@ module Webmoney
       wmid = '000000000007'
       created_at = Time.at(1077735241.37)
       res = @wm.request(:get_passport, :wmid => wmid, :dict => 1)
-      res.should == {
-        :full_access=>false,
-        :wmids => {
-          wmid => {
-            :created_at=>Time.at(1077733193.353)
-          }
+      res[:full_access].should be_false
+      res[:wmids].should == {
+        wmid => { :created_at=>Time.at(1077733193.353) }
+      }
+      res[:attestat].should == {
+        :regcid=>"10",
+        :locked=>"1",
+        :recalled=>"0",
+        :cid=>"103453",
+        :admlocked=>"0",
+        :created_at=>created_at,
+        :datecrt=>"2004-02-25T21:54:01.370",
+        :regnickname=>"WM Passport Service /Центр аттестации/",
+        :regwmid=>"464889785562",
+        :attestat=>150,
+        :tid=>"150",
+        :datediff=>(Date.today - created_at.send(:to_date)).to_s
+      }
+
+      u_info = {
+        :locked=>"0",
+        :ctype=>"1",
+        :citid=>"12918",
+        :cap_owner=>"0",
+        :region=>"Москва",
+        :countryid=>"195",
+        :city=>"Москва",
+        :pasdoc=>"0",
+        :nickname=>"Арбитр",
+        :country=>"Россия",
+        :inndoc=>"0",
+        :email=>"", :pdateMMDDYYYY => "", :pday => "", :pmonth => "", :pyear => "",
+        :iname=>"", :inn=>"", :okonx=>"", :bik=>"", :pbywhom=>"", :phonemobile=>"", :rcountry=>"",
+        :bmonth=>"", :jadres=>"", :okpo=>"", :bday=>"", :pnomer=>"", :bankname=>"", :pcountry=>"", :pcountryid=>"",
+        :jcountryid=>"", :ks=>"", :infoopen=>"", :icq=>"", :byear=>"", :oname=>"", :osnovainfo=>"", :dirfio=>"",
+        :pdate=>"", :bplace=>"", :rs=>"", :rcity=>"", :adres=>"", :phone=>"", :buhfio=>"", :radres=>"", :fname=>"",
+        :phonehome=>"", :jcity=>"", :name=>"", :pcity=>"", :jstatus=>"", :fax=>"", :zipcode=>"", :rcountryid=>"",
+        :web=>"", :jzipcode=>"", :jcountry=>""
+      }
+
+#      a1 = res[:userinfo].keys.map(&:to_s).sort
+#      a2 = u_info.keys.map(&:to_s).sort
+#      puts ((a1|a2) - (a1 & a2)).inspect
+
+      res[:userinfo].should == u_info
+
+      res[:directory].should == {
+        :ctype=>{
+          1=>"Частное лицо",
+          2=>"Юридическое лицо"
         },
-        :attestat=>{
-          :regcid=>"10",
-          :locked=>"1",
-          :recalled=>"0",
-          :cid=>"103453",
-          :admlocked=>"0",
-          :created_at=>created_at,
-          :datecrt=>"2004-02-25T21:54:01.370",
-          :regnickname=>"WM Passport Service /Центр аттестации/",
-          :regwmid=>"464889785562",
-          :attestat=>150,
-          :tid=>"150",
-          :datediff=>(Date.today - created_at.send(:to_date)).to_s
+        :jstatus=>{
+          20=>"Директор юридического лица",
+          21=>"Бухгалтер юридического лица",
+          22=>"Представитель юридического лица",
+          23=>"ИП"
         },
-        :userinfo=>{
-          :locked=>"0",
-          :ctype=>"1",
-          :citid=>"12918",
-          :cap_owner=>"0",
-          :region=>"Москва",
-          :countryid=>"195",
-          :city=>"Москва",
-          :pasdoc=>"0",
-          :nickname=>"Арбитр",
-          :country=>"Россия",
-          :inndoc=>"0",
-          :email=>"", :iname=>"", :inn=>"", :okonx=>"", :bik=>"", :pbywhom=>"", :phonemobile=>"", :rcountry=>"",
-          :bmonth=>"", :jadres=>"", :okpo=>"", :bday=>"", :pnomer=>"", :bankname=>"", :pcountry=>"", :pcountryid=>"",
-          :jcountryid=>"", :ks=>"", :infoopen=>"", :icq=>"", :byear=>"", :oname=>"", :osnovainfo=>"", :dirfio=>"",
-          :pdate=>"", :bplace=>"", :rs=>"", :rcity=>"", :adres=>"", :phone=>"", :buhfio=>"", :radres=>"", :fname=>"",
-          :phonehome=>"", :jcity=>"", :name=>"", :pcity=>"", :jstatus=>"", :fax=>"", :zipcode=>"", :rcountryid=>"",
-          :web=>"", :jzipcode=>"", :jcountry=>""},
-        :directory=>{
-          :ctype=>{
-            1=>"Частное лицо",
-            2=>"Юридическое лицо"
-          },
-          :jstatus=>{
-            20=>"Директор юридического лица",
-            21=>"Бухгалтер юридического лица",
-            22=>"Представитель юридического лица",
-            23=>"ИП"
-          },
-          :types=>{
-            100=>"Аттестат псевдонима",
-            110=>"Формальный аттестат",
-            120=>"Начальный аттестат",
-            130=>"Персональный аттестат",
-            135=>"Аттестат продавца",
-            136=>"Аттестат Capitaller",
-            140=>"Аттестат разработчика",
-            150=>"Аттестат регистратора",
-            170=>"Аттестат Гаранта",
-            190=>"Аттестат сервиса WMT",
-            200=>"Аттестат сервиса WMT",
-            300=>"Аттестат Оператора"
-          }
+        :types=>{
+          100=>"Аттестат псевдонима",
+          110=>"Формальный аттестат",
+          120=>"Начальный аттестат",
+          130=>"Персональный аттестат",
+          135=>"Аттестат продавца",
+          136=>"Аттестат Capitaller",
+          140=>"Аттестат разработчика",
+          150=>"Аттестат регистратора",
+          170=>"Аттестат Гаранта",
+          190=>"Аттестат сервиса WMT",
+          200=>"Аттестат сервиса WMT",
+          300=>"Аттестат Оператора"
         }
       }
     end
