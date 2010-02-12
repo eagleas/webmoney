6137725135d207d6e0d005693478aaa57eb39f33
Alex Eagle <eagle.alex@gmail.com>
2010-02-12 12:15:53 +0300

    Stub X11 request in NonExistentWMID test

--- spec/unit/passport_spec.rb
+++ spec/unit/passport_spec.rb
@@ -121,6 +121,7 @@ module Webmoney
     end
 
     it "should raise exception on non existent WMID" do
+      @wm.stub!(:http_request).and_return("<?xml version='1.0' encoding='windows-1251'?><response retval='0'><fullaccess>0</fullaccess><certinfo wmid='012345678901'/><retdesc>WMID not found</retdesc></response>")
       lambda {@wm.request(:get_passport, :wmid => '012345678901')}.should raise_error(Webmoney::NonExistentWmidError)
     end
 
